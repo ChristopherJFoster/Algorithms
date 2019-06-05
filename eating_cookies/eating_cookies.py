d77f98414c0e6f82059754b723d10f2d1d0713f5
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-05 18:36:54 -0400

    Completed eating_cookies.py. It turn s out my code already worked for large ns, but I wrote it so it didn't need a cache passed in initially. I  added two lines to fix this. See comments in eating_ cookies.py.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,7 +7,12 @@ import sys
 # recursive solution
 
 
+# This solution actually works for larger ns, but I wrote not to accept a cache as an argument (which I think is better than requiring a cache). However, the tests in test_eating_cookies.py _do_ pass in a cache, which breaks my code. I added two lines to reject the incoming cache.
+
 def eating_cookies(n, cache={0: 1, 1: 1, 2: 2}):
+    # The following two lines reject any cache passed in as an argument:
+    if cache[0] == 0:
+        cache = {0: 1, 1: 1, 2: 2}
     if n in cache:
         return cache[n]
     else:
@@ -23,3 +28,6 @@ if __name__ == "__main__":
             ways=eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
+
+
+print(eating_cookies(50))
