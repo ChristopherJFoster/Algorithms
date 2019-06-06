f312579c51143804d5b149b4554d5b223db53315
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-06 19:24:37 -0400

    I couldn't get the knapsack problem solved, but I'm submitting my work so far.

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,56 +6,58 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 
-# if __name__ == '__main__':
-#     if len(sys.argv) > 1:
-#         capacity = int(sys.argv[2])
-#         file_location = sys.argv[1].strip()
-#         file_contents = open(file_location, 'r')
-#         items = []
-
-#         for line in file_contents.readlines():
-#             data = line.rstrip().split()
-#             items.append(Item(int(data[0]), int(data[1]), int(data[2])))
-
-#         file_contents.close()
-#         print(knapsack_solver(items, capacity))
-#     else:
-#         print('Usage: knapsack.py [filename] [capacity]')
-
-
 def knapsack_solver(items, capacity):
-    max = []
+    maxes = {}
 
     for i in range(len(items)):
-        def add(j, combo={'Chosen': [], 'Size': 0, 'Value': 0}):
-            # print(combo['Size'] + items[j].size, capacity)
-            if combo['Size'] + items[j].size <= capacity:
-                combo['Chosen'].append(items[j].index)
-                combo['Size'] += items[j].size
-                combo['Value'] += items[j].value
-
-            if j == len(items) - 1:
-                print('appended combo: ', combo)
-                max.append(combo)
-
+        maxes[i] = {}
+        for j in range(1, capacity + 1):
+            if i == 0:
+                if items[i].size <= j:
+                    maxes[i][j] = {'Chosen': [items[i].index],
+                                   'Value': items[i].value}
+                else:
+                    maxes[i][j] = {'Chosen': [], 'Value': 0}
+            elif j - items[i].size == 0:
+                if maxes[i - 1][j]['Value'] > items[i].value:
+                    maxes[i][j] = {'Chosen': maxes[i - 1][j]['Chosen'],
+                                   'Value': maxes[i - 1][j]['Value']}
+                else:
+                    maxes[i][j] = {'Chosen': [items[i].index],
+                                   'Value': items[i].value}
+            elif j - items[i].size > 0:
+                if maxes[i - 1][j]['Value'] > items[i].value + maxes[i - 1][j - items[i].size]['Value']:
+                    maxes[i][j] = {'Chosen': maxes[i - 1][j]['Chosen'],
+                                   'Value': maxes[i - 1][j]['Value']}
+                else:
+                    new_max = {'Chosen': maxes[i - 1][j - items[i].size]['Chosen'],
+                               'Value': maxes[i - 1][j - items[i].size]['Value']}
+                    print('new_max: ', new_max)
+                    new_max['Chosen'].append(items[i].index)
+                    new_max['Value'] += items[i].value
+                    print('new_max: ', new_max)
+                    maxes[i][j] = new_max
+                    print('maxes[i][j]: ', maxes[i][j])
             else:
-                print('else')
-                for k in range(j + 1, len(items)):
-                    print('passed combo: ', combo, k, i)
-                    add(k, combo)
-
-        add(i)
-
-        # Compare previous max value combo with every combo of items assuming items[i] as a starting point and set new max equal to the highest value combo.
-
-        max = [sorted(max, key=lambda item: item['Value'], reverse=True)[0]]
-
-    # cleans up output for tests
-    max = {'Chosen': max[0]['Chosen'], 'Value': max[0]['Value']}
-    return max
+                maxes[i][j] = {'Chosen': [], 'Value': 0}
+    # for i in maxes:
+    #     for j in maxes[i]:
+    #         print(i, j, maxes[i][j])
+    return maxes[len(items) - 1][capacity]
 
 
-itemlist = [Item(index=1, size=4, value=8), Item(
-    index=2, size=4, value=2), Item(index=3, size=6, value=6)]
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        capacity = int(sys.argv[2])
+        file_location = sys.argv[1].strip()
+        file_contents = open(file_location, 'r')
+        items = []
 
-print('final answer: ', knapsack_solver(itemlist, 10))
+        for line in file_contents.readlines():
+            data = line.rstrip().split()
+            items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+
+        file_contents.close()
+        print(knapsack_solver(items, capacity))
+    else:
+        print('Usage: knapsack.py [filename] [capacity]')
